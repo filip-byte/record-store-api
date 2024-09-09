3c4137bb5f209d9f34e0db8eed6c9f016c360c1a
filip-byte <ohboytoby@gmail.com>
2024-09-09 07:51:21 +0100

    feat: add AuthorService implementation

---
+++ src/main/java/com/filipbyte/record_shop_api/service/AlbumServiceImpl.java
@@ -0,0 +1,51 @@
+package com.filipbyte.record_shop_api.service;
+
+import com.filipbyte.record_shop_api.model.Author;
+import com.filipbyte.record_shop_api.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        List<Author> authors = (List<Author>) authorRepository.findAll();
+        return new ResponseEntity<>(authors, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Author> getAuthorById(Long id) {
+        return authorRepository.findById(id)
+                .map(author -> new ResponseEntity<>(author, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public ResponseEntity<Author> createAuthor(Author author) {
+        Author savedAuthor = authorRepository.save(author);
+        return new ResponseEntity<>(savedAuthor, HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Author> updateAuthor(Long id, Author authorDetails) {
+        return authorRepository.findById(id)
+                .map(author -> {
+                    author.setName(authorDetails.getName());
+                    Author updatedAuthor = authorRepository.save(author);
+                    return new ResponseEntity<>(updatedAuthor, HttpStatus.OK);
+                })
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    }
