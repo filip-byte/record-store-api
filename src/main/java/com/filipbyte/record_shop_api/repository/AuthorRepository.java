292c0085c32264db29c54feaa9d4dcbcda6631ea
filip-byte <ohboytoby@gmail.com>
2024-09-11 16:17:40 +0100

    feat: add findAuthorByName method to Author Repository

--- src/main/java/com/filipbyte/record_shop_api/repository/AuthorRepository.java
+++ src/main/java/com/filipbyte/record_shop_api/repository/AuthorRepository.java
@@ -1,9 +1,13 @@
 package com.filipbyte.record_shop_api.repository;
 
 import com.filipbyte.record_shop_api.model.Author;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AuthorRepository extends CrudRepository<Author, Long> {}
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findAuthorByName(String name);
+}
 
