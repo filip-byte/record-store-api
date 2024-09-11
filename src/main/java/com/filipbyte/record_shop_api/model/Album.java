30b336bc79d1992e01208504fc93f38e6db09dec
filip-byte <ohboytoby@gmail.com>
2024-09-11 16:24:09 +0100

    chore: clean up the imports for Album.class

--- src/main/java/com/filipbyte/record_shop_api/model/Album.java
+++ src/main/java/com/filipbyte/record_shop_api/model/Album.java
@@ -1,7 +1,6 @@
 package com.filipbyte.record_shop_api.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
