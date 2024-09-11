e05a2f48286f2067e3c321b8f11ae7323b457354
filip-byte <ohboytoby@gmail.com>
2024-09-11 16:21:21 +0100

    fix: replace authorService autorwiring with service implementation

--- src/main/java/com/filipbyte/record_shop_api/controller/AuthorController.java
+++ src/main/java/com/filipbyte/record_shop_api/controller/AuthorController.java
@@ -1,8 +1,7 @@
 package com.filipbyte.record_shop_api.controller;
 
 import com.filipbyte.record_shop_api.model.Author;
-import com.filipbyte.record_shop_api.service.AuthorService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.filipbyte.record_shop_api.service.AuthorServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,35 +12,31 @@ import java.util.List;
 public class AuthorController {
 
     // todo: we need authorService implementation here
-    private final AuthorService authorService;
-
-    @Autowired
-    public AuthorController(AuthorService authorService) {
-        this.authorService = authorService;
-    }
+    private AuthorServiceImpl authorServiceImpl;
 
     @GetMapping
     public ResponseEntity<List<Author>> getAllAuthors() {
-        return authorService.getAllAuthors();
+        return authorServiceImpl.getAllAuthors();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
-        return authorService.getAuthorById(id);
+        return authorServiceImpl.getAuthorById(id);
     }
 
     @PostMapping
     public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
-        return authorService.createAuthor(author);
+        return authorServiceImpl.createAuthor(author);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Author> updateAuthor(@PathVariable Long id, @RequestBody Author authorDetails) {
-        return authorService.updateAuthor(id, authorDetails);
+        return authorServiceImpl.updateAuthor(id, authorDetails);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
-        return authorService.deleteAuthor(id);
+        return authorServiceImpl.deleteAuthor(id);
     }
+
 }
