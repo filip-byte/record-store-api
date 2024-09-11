b161793e7d880f545af9aa1fe89953061e915e1e
filip-byte <ohboytoby@gmail.com>
2024-09-11 16:23:16 +0100

    refactor: cleaned up Jackson annotations for ManyToOne relationship

--- src/main/java/com/filipbyte/record_shop_api/model/Album.java
+++ src/main/java/com/filipbyte/record_shop_api/model/Album.java
@@ -1,15 +1,12 @@
 package com.filipbyte.record_shop_api.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
-
-import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Table(name = "albums")
@@ -34,9 +31,12 @@ public class Album {
 
     private Integer stock;
 
-    @ManyToOne
-    @JoinColumn(name = "author_id", nullable = false)
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
     @JsonBackReference
     private Author author;
 
+    public Author getAuthor() {
+        return author;
+    }
 }
